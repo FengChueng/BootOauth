06c29c8e006f73212319cd1b41dc097b58eb9fb6
FengChueng <yinglongChueng@gmail.com>
2018-06-07 18:47:28 +0800

    初始化

---
+++ OauthServer/src/main/java/com/zyl/Application.java
@@ -0,0 +1,35 @@
+package com.zyl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * An opinionated WebApplicationInitializer to run a SpringApplication from a
+     * traditional WAR deployment. Binds Servlet, Filter and
+     * ServletContextInitializer beans from the application context to the servlet
+     * container.
+     *
+     * @link http://docs.spring.io/spring-boot/docs/current/api/index.html?org/springframework/boot/context/web/SpringBootServletInitializer.html
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    
+
+}
